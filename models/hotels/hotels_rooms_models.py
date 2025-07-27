864ab2d5295033e7e03c90db4f1305717fb537c3
Vladislav Krivenko <vladdante95@gmail.com>
2025-07-27 09:16:43 +0300

    fix

--- models/hotels/hotels_rooms_models.py
+++ models/hotels/hotels_rooms_models.py
@@ -2,13 +2,14 @@ from datetime import date
 from typing import Optional
 
 class Room:
-    def __init__(self, hotel_id: int, hotel_name: str, room_id: int, room_name: str, room_description: str, price: int):
+    def __init__(self, hotel_id: int, hotel_name: str, room_id: int, room_name: str, room_description: str):
+    #def __init__(self, hotel_id: int, hotel_name: str, room_id: int, room_name: str, room_description: str, price: int):
         self.hotel_id = hotel_id
         self.hotel_name = hotel_name
         self.room_id = room_id
         self.room_name = room_name
         self.room_description = room_description
-        self.price = price
+        #self.price = price
 
     def to_dict(self):
         return {
